03e08415359668441a65eabe786f909aafca545e
alra97 <alethear2020@gmail.com>
2024-06-18 19:47:16 +0530

    coins

--- lib/game/dino_run.dart
+++ lib/game/dino_run.dart
@@ -32,6 +32,7 @@ class DinoRun extends FlameGame with TapDetector, HasCollisionDetection {
     'parallax/plx-4.png',
     'parallax/plx-5.png',
     'parallax/plx-6.png',
+    'coins.png'
   ];
 
   // List of all the audio assets.
